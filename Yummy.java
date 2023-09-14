b5f86b54a2709c44eb5d10660647a86665486efc
carletonberry <carletonberry@billericak12.com>
2023-09-14 09:40:46 -0400

    initial commit

--- Yummy.java
+++ Yummy.java
@@ -2,10 +2,23 @@ public class Yummy {
     public static void main(String [] args){
  System.out.println( "Yummy");
  chocolate();       //call the chocolate method
+ icecream();
+ System.out.println("done");
     }
     
     public static void chocolate(){
 System.out.print("Chocolate");
 System.out.println("Cake");
     }
+    public static void frenchfries(){
+        System.out.println( "french fries with ketchup");
+        chocolate();
+        System.out.println( "that was yummy");
+    }
+public static void icecream(){
+System.out.println("ice");
+frenchfries();
+System.out.println("cream");
 }
+}
+
